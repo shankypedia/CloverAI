877f869aaa9fc53020efb07652b256217176b602
Sashank <hello@sashank.wiki>
2024-12-02 11:31:29 +0530

    Create __init__.py

---
+++ privacy_protection/__init__.py
@@ -0,0 +1,13 @@
+from .privacy_protection import (
+    PrivacyProtector,
+    generate_key,
+    encrypt_data,
+    anonymize_data
+)
+
+__all__ = [
+    'PrivacyProtector',
+    'generate_key',
+    'encrypt_data',
+    'anonymize_data'
+]
