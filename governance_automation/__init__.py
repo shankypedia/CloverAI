2ec6354aad5a8d39685a60d8933dd1c2059fbefb
Sashank <hello@sashank.wiki>
2024-12-02 11:46:53 +0530

    Create __init__.py

---
+++ governance_automation/__init__.py
@@ -0,0 +1,4 @@
+from .governance_automation import enforce_policy, GovernanceEnforcer
+from .governance_scaler import GovernanceScaler
+
+__all__ = ['enforce_policy', 'GovernanceEnforcer', 'GovernanceScaler']
