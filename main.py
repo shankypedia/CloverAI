99d2df2d1d15393119c4172cf4fc56cb657dbd89
Sashank <hello@sashank.wiki>
2025-01-06 19:37:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import logging
 from typing import Any, Dict
 import pandas as pd
 from datetime import datetime
-import json
 from rich.console import Console
 from rich.panel import Panel
 from rich.table import Table
